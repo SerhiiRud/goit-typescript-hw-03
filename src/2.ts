51ade4c98009da9321d631608af1fafabc8e3813
SerhiiRud <carmagsales@gmail.com>
2023-10-16 12:24:08 +0300

    done

--- src/2.ts
+++ src/2.ts
@@ -16,11 +16,12 @@
 
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
 
-  constructor(name: string, department: string, salary: number) {
+  constructor(
+    public name: string,
+    private department: string,
+    protected salary: number
+  ) {
     this.name = name;
     this.department = department;
     this.salary = salary;
@@ -32,8 +33,10 @@ class Employee {
 }
 
 class Manager extends Employee {
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary + 10000);
+  }
   // Реалізуйте конструктор та збільшіть salary на 10000
 }
-
 
 export {};
--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,45 @@
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean;
+  protected tenants: Person[];
+
+  constructor(protected key: Key) {
+    this.key = key;
+  }
+
+  comeIn(tenant: Person): void {
+    if (this.door) this.tenants.push(tenant);
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) this.door = true;
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +48,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
